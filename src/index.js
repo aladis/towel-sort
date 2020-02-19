0ad2f64b4d115a065bac46fcad63236500f7060a
aladis <vlmandzhiev@gmail.com>
2020-02-19 20:43:19 +0300

    feat: decision task Towel Sort

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix) {
+    if (!matrix || matrix.length === 0) return [];
+    for (let x = 1; x < matrix.length; x += 2) {
+        matrix[x] = matrix[x].reverse();
+    }
+    return matrix.flat();
+};
